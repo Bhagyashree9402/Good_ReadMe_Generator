336eaf408e6357edd3cdf7cb928525640ac033a7
Bhagyashree <bhagyashree9402@gmail.com>
2020-05-31 16:47:32 -0700

    did some changes in code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
-const token = "d2204d4ef5bf4183fc0a0a9ddd1e261949c38116";
+const token = "566ab5f3ee05bd33431036ffe6180a876ba8ebfa";
 
 inquirer.prompt({
     message: "Enter your github username",
@@ -79,112 +79,121 @@ inquirer.prompt({
             })
 
 
-            fs.appendFile("readme.md", "\n" + "![alt text](https://img.shields.io/badge/license-" + response.license + "-green)" + "\n", function (error) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    console.log("success");
+            fs.appendFile("readme.md", "\n" + "![alt text](https://img.shields.io/badge/license-" + response.license + "-green)" + "\n"
+                + "\n" + "![alt text](" + gitProfile + ") \n"
+                + "\n" + "### email:" + gitEmail + "\n"
+                + "\n" + "## Description" + "\n" + response.description + "\n"
+                + "\n" + "## Table of Content" + "\n" + "- License" + "\n" + "- Usage" + "\n" + "- Installation" + "\n" + "- Contribution" + "\n"
+                + "\n" + "## License" + "\n" + response.license + "\n"
+                + "\n" + "## Usage" + "\n" + response.usage + "\n"
+                + "\n" + "## Installation" + "\n" + response.installation + "\n"
+                + "\n" + "## Contribution" + "\n" + response.contribution + "\n"
+                + "\n" + "## tests" + "\n" + response.test + "\n", function (error) {
+                    if (error) {
+                        console.log(error);
+                    }
+                    else {
+                        console.log("success");
 
-                }
+                    }
 
-            })
-
-
-            fs.appendFile("readme.md", "\n" + "![alt text](" + gitProfile + ") \n", function (error) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    console.log("success");
-
-                }
-
-            })
-
-            fs.appendFile("readme.md", "\n" + "### email:" + gitEmail + "\n", function (error) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    console.log("success");
-
-                }
-
-            })
+                })
 
 
+            // fs.appendFile("readme.md", "\n" + "![alt text](" + gitProfile + ") \n", function (error) {
+            //     if (error) {
+            //         console.log(error);
+            //     }
+            //     else {
+            //         console.log("success");
 
-            fs.appendFile("readme.md", "\n" + "## Description" + "\n" + response.description + "\n", function (error) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    console.log("success");
+            //     }
 
-                }
-            })
+            // })
 
-            fs.appendFile("readme.md", "\n" + "## Table of Content" + "\n" + "- License" + "\n" + "- Usage" + "\n" + "- Installation" + "\n" + "- Contribution" + "\n", function (error) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    console.log("success");
+            // fs.appendFile("readme.md", "\n" + "### email:" + gitEmail + "\n", function (error) {
+            //     if (error) {
+            //         console.log(error);
+            //     }
+            //     else {
+            //         console.log("success");
 
-                }
-            })
+            //     }
+
+            // })
 
 
-            fs.appendFile("readme.md", "\n" + "## License" + "\n" + response.license + "\n", function (error) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    console.log("success");
 
-                }
-            })
+            // fs.appendFile("readme.md", "\n" + "## Description" + "\n" + response.description + "\n", function (error) {
+            //     if (error) {
+            //         console.log(error);
+            //     }
+            //     else {
+            //         console.log("success");
 
-            fs.appendFile("readme.md", "\n" + "## Usage" + "\n" + response.usage + "\n", function (error) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    console.log("success");
+            //     }
+            // })
 
-                }
-            })
+            // fs.appendFile("readme.md", "\n" + "## Table of Content" + "\n" + "- License" + "\n" + "- Usage" + "\n" + "- Installation" + "\n" + "- Contribution" + "\n", function (error) {
+            //     if (error) {
+            //         console.log(error);
+            //     }
+            //     else {
+            //         console.log("success");
 
-            fs.appendFile("readme.md", "\n" + "## Installation" + "\n" + response.installation + "\n", function (error) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    console.log("success");
+            //     }
+            // })
 
-                }
-            })
 
-            fs.appendFile("readme.md", "\n" + "## Contribution" + "\n" + response.contribution + "\n", function (error) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    console.log("success");
+            // fs.appendFile("readme.md", "\n" + "## License" + "\n" + response.license + "\n", function (error) {
+            //     if (error) {
+            //         console.log(error);
+            //     }
+            //     else {
+            //         console.log("success");
 
-                }
-            })
+            //     }
+            // })
 
-            fs.appendFile("readme.md", "\n" + "## tests" + "\n" + response.test + "\n", function (error) {
-                if (error) {
-                    console.log(error);
-                }
-                else {
-                    console.log("success");
+            // fs.appendFile("readme.md", "\n" + "## Usage" + "\n" + response.usage + "\n", function (error) {
+            //     if (error) {
+            //         console.log(error);
+            //     }
+            //     else {
+            //         console.log("success");
 
-                }
-            })
+            //     }
+            // })
+
+            // fs.appendFile("readme.md", "\n" + "## Installation" + "\n" + response.installation + "\n", function (error) {
+            //     if (error) {
+            //         console.log(error);
+            //     }
+            //     else {
+            //         console.log("success");
+
+            //     }
+            // })
+
+            // fs.appendFile("readme.md", "\n" + "## Contribution" + "\n" + response.contribution + "\n", function (error) {
+            //     if (error) {
+            //         console.log(error);
+            //     }
+            //     else {
+            //         console.log("success");
+
+            //     }
+            // })
+
+            // fs.appendFile("readme.md", "\n" + "## tests" + "\n" + response.test + "\n", function (error) {
+            //     if (error) {
+            //         console.log(error);
+            //     }
+            //     else {
+            //         console.log("success");
+
+            //     }
+            // })
 
         })
     })
--- readme.md
+++ readme.md
@@ -1,11 +1,13 @@
 # Good Read Me Generator
 
+![alt text](https://img.shields.io/badge/license-Apache-green)
+
 ![alt text](https://avatars3.githubusercontent.com/u/61488735?v=4) 
 
-![alt text](https://img.shields.io/badge/license-MIT-green)
+### email:bhagyashree9402@gmail.com
 
 ## Description
-to spend more time working on project and less time creating the readme 
+allows to spend more time working on project and less time on creating readme
 
 ## Table of Content
 - License
@@ -13,19 +15,17 @@ to spend more time working on project and less time creating the readme
 - Installation
 - Contribution
 
+## License
+Apache
+
+## Usage
+Can easily create the readme file
+
 ## Installation
 npm install
 
-### email:bhagyashree9402@gmail.com
-
 ## Contribution
-single contributor
+single contribution
 
 ## tests
 unit test
-
-## License
-MIT
-
-## Usage
-can easily create the good read me
